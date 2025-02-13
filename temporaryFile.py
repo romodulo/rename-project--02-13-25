8563081462e5c9b04d5ec594716a87f5c0cf83d6
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-02-13 23:59:32 +1000

    First commit

---
+++ temporaryFile.py
@@ -0,0 +1,10 @@
+#! /home/romel-linux/untitledProjects/.myenv_main/bin/python
+#shebang works{{all that is needed
+#is a description for the program}}
+
+import os
+from pprint import pprint
+#import send2trash below
+from secretkeys import secret_key_1, secret_key_2
+
+print('yo.')
